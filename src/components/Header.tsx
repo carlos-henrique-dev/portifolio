4bd612ef36adf73d629466119a28ee7ee4bd42c9
Carlos Henrique <henrique@MyAvell.localdomain>
2021-11-15 22:19:46 -0400

    adiciona anchor dinamico

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,37 +3,46 @@ import { useTranslation } from 'next-i18next';
 import Link from 'next/link'
 
 import LocaleSelect from './LocaleSelect'
+import { useRouter } from 'next/router';
 
 const Header = () => {
+	const router = useRouter()
 	const { t } = useTranslation('header');
+
 	const [showMenu, setShowMenu] = useState(false)
 
 	const toggleMenu = () => setShowMenu(!showMenu)
 
+	const getAnchor = (anchor) => {
+		return router.pathname === '/' ? anchor : `/${anchor}`
+	}
+
 	return (
 		<header className="header">
 			<div className="logo-wrapper">
-				<a href="/" className="logo">
+				<Link href={getAnchor('')}  passHref>
+					<a href="" className="logo">
 					<span className="first-name">CARLOS</span>
 					<span className="last-name">HENRIQUE</span>
-				</a>
+					</a>
+				</Link>
 			</div>
 			<nav className="navbar">
 				<ul className={`navlinks ${showMenu && 'active'}`} >
 					<li className="link" onClick={() => setShowMenu(false)}>
-						<Link href="#home">{t('home')}</Link>
+						<Link href={getAnchor("#home")}>{t('home')}</Link>
 					</li>
 					<li className="link" onClick={() => setShowMenu(false)}>
-						<Link href="#aboutme">{t('aboutme')}</Link>
+						<Link href={getAnchor("#aboutme")}>{t('aboutme')}</Link>
 					</li>
 					<li className="link" onClick={() => setShowMenu(false)}>
-						<Link href="#projects">{t('projects')}</Link>
+						<Link href={getAnchor("#projects")}>{t('projects')}</Link>
 					</li>
 					<li className="link" onClick={() => setShowMenu(false)}>
-						<Link href="/blog">{t('blog')}</Link>
+						<Link href={getAnchor("/blog")}>{t('blog')}</Link>
 					</li>
 					<li className="link" onClick={() => setShowMenu(false)}>
-						<Link href="#contact">{t('contact')}</Link>
+						<Link href={getAnchor("#contact")}>{t('contact')}</Link>
 					</li>
 					<li className="link">
 						<LocaleSelect />
