cc20077eb2cae42d2835271fe78e5557a8c0c8fe
Carlos Henrique <carloshenrique@Carloss-MacBook-Pro.local>
2024-08-30 10:32:18 -0400

    Completely refactoring the portifolio

--- src/pages/contact.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function contact() {
-  return <div>Página em desenvolvimento</div>;
-}
-
-export default contact;
--- src/pages/projects/dot-movies.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function acaProject() {
-  return <div>Página em desenvolvimento</div>;
-}
-
-export default acaProject;
